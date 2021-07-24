caee8bb943aa1510ddeacf1d65596cccb1bf7bb8
Ainur-Dzholdosheva <ajnurdzoldoseva@gmail.com>
2021-07-24 23:21:20 +0600

    admin panel use

--- places/admin.py
+++ places/admin.py
@@ -2,4 +2,14 @@ from django.contrib import admin
 from .models import Place, FeedBack
 
 admin.site.register(Place)
-admin.site.register(FeedBack)
+
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = ['text', 'place', 'user', 'checked']
+    list_editable = ['checked']
+    list_filter = ['checked']
+    search_fields = ['text', 'place__name', 'place__description', 'place__location']
+
+    fields = ['user', 'place', 'text']
+    readonly_fields = ['place', 'text']
+
+admin.site.register(FeedBack, FeedbackAdmin)
